30ef4d8eb51892dc3757f8194f3c6da7ca60b23c
ShahinShabnam <shabnam.shahin@rancelab.com>
2017-08-29 18:09:54 +0530

    by clicking go to specific language page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule, Http } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { TranslateModule, TranslateLoader, TranslateStaticLoader, MissingTranslationHandler } from 'ng2-translate';
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { MyMissingTranslationHandler } from './missingtemplate.component';
 
+
+export function createTranslateLoader(http: Http) {
+  return new TranslateStaticLoader(http, './assets/i18n', '.json');
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent},
+      { path: 'about', component: AboutComponent},
+      { path: 'contact', component: ContactComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    ]),
+    TranslateModule.forRoot({
+      provide: TranslateLoader,
+      useFactory: (createTranslateLoader),
+      deps: [Http]
+    })
   ],
-  providers: [],
+  providers: [
+    { provide: MissingTranslationHandler, useClass: MyMissingTranslationHandler}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
